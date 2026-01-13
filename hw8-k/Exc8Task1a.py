137745ee5e4740d749c81b6cf593e78cd73fdb31
KatherineCaley <katherine.e.caley@gmail.com>
2026-01-13 09:22:31 +0100

    NEW: task1a finished

--- hw8-k/Exc8Task1a.py
+++ hw8-k/Exc8Task1a.py
@@ -4,9 +4,10 @@ from numpy.linalg import inv
 
 # values/constants
 vars = [2.27, 2.26, 1.14, 1.70, 6.5]
-t = numpy.array([1, 2.5, 5, 7.5, 10]).T
-y = numpy.array([2.78, 6.57, 10.57, 14.9, 27.1]).T
-theta_0 = numpy.loadtxt("Input.txt", ndmin=2)
+t = numpy.array([1, 2.5, 5, 7.5, 10])
+y = numpy.array([2.78, 6.57, 10.57, 14.9, 27.1])
+theta_0 = numpy.loadtxt("Input.txt").astype(float)
+EPS = 10e-8
 
 
 def xi(theta, ti) -> float:
@@ -21,30 +22,43 @@ def dxi_dk1(ti, theta) -> float:
 
 def dxi_dk2(ti, theta) -> float:
     k1, k2 = theta
-    return k1 * ti * numpy.exp(k2 * ti)
+    return (k1 * ti * numpy.exp(k2 * ti))
 
 
-def weight_matrix(vars):
+def weight_matrix(vars) -> numpy.ndarray:
     w = numpy.zeros((len(vars), len(vars)))
     inv_vars = [1 / x for x in vars]
-    return numpy.fill_diagonal(w, inv_vars)
+    numpy.fill_diagonal(w, inv_vars)
+    return w
 
 
-def jacobian(theta, t):
+def jacobian(theta, t) -> numpy.ndarray:
     n_data = len(t)
     n_params = len(theta)
-    jcbn = numpy.empty((n_data, n_params))
+    jcbn = numpy.empty((n_data, n_params), dtype=float)
 
     for i in range(n_data):
-        jcbn[(i, 0)] = dxi_dk1(ti=t[i], theta=theta)
-        jcbn[(i, 1)] = dxi_dk2(ti=t[i], theta=theta)
+        jcbn[(i, 0)] = float(dxi_dk1(ti=t[i], theta=theta))
+        jcbn[(i, 1)] = float(dxi_dk2(ti=t[i], theta=theta))
     return jcbn
 
 
-def model(t, theta_s):
+def gauss_newton_step(t, theta_s, x):
     J = jacobian(theta=theta_s, t=t)
     W = weight_matrix(vars=vars)
 
-    return 
+    return theta_s - inv(J.T @ W @ J) @ J.T @ W @ (x - y)
 
+with open("Exc8Task1a.txt", mode="w") as outfile:
+    theta_s = theta_0
+    diff = numpy.inf
+    iterations = 0
 
+    while diff > EPS and iterations < 30:
+        x = numpy.array([xi(theta_s, ti=ti) for ti in t])
+        theta_s_plus_1 = gauss_newton_step(t=t, theta_s=theta_s, x=x)
+        diff = numpy.linalg.norm(theta_s_plus_1 - theta_s, 1)
+        theta_s = theta_s_plus_1
+        iterations += 1
+
+    outfile.write(f"{theta_s[0]:.2f}\n{theta_s[1]:.2f}\n")
